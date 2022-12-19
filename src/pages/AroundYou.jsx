f6d1a748675873def5101b5332fdc24c7ad707f1
jiahoe <mrjiahoe@hotmail.com>
2022-12-19 15:16:57 +0800

    fetch error in around you

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,5 +1,71 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
 
-const CountryTracks = () => <div>CountryTracks</div>;
+import { Error, Loader, SongCard } from '../components';
+import { useGetSongsByCountryQuery } from '../redux/services/shazamCore';
+
+const CountryTracks = () => {
+	const [country, setCountry] = useState('');
+	const [loading, setLoading] = useState(true);
+	const { activeSong, isPlaying } = useSelector((state) => state.player);
+	const { data, isFetching, error } = useGetSongsByCountryQuery(country);
+
+	const options = {
+		method: 'GET',
+		url: 'https://geo.ipify.org/api/v2/country',
+		params: {
+			apiKey: 'at_x9r8iAxXiQyHUbBPBJvaGKEN31Bl3',
+			ipAddress: '8.8.8.8',
+		},
+	};
+
+	// useEffect(() => {
+	// 	axios
+	// 		.get(
+	// 			'https://geo.ipify.org/api/v2/country?apiKey=at_x9r8iAxXiQyHUbBPBJvaGKEN31Bl3&ipAddress=8.8.8.8'
+	// 		)
+	// 		.then((res) => console.log(res.json()))
+	// 		.catch((err) => console.log(err))
+	// 		.finally(() => setLoading(false));
+	// }, [country]);
+
+	useEffect(() => {
+		axios
+			.request(options)
+			.then(function (response) {
+				console.log(response.data);
+			})
+			.catch(function (error) {
+				console.error(error);
+			});
+	}, []);
+
+	if (isFetching && loading)
+		return <Loader title="Loading Songs around you..." />;
+
+	if (error && country !== '') return <Error />;
+
+	return (
+		<div className="flex flex-col">
+			<h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+				Around you <span className="font-black">{country}</span>
+			</h2>
+
+			<div className="flex flex-wrap sm:justify-start justify-center gap-8">
+				{data?.map((song, i) => (
+					<SongCard
+						key={song.key}
+						song={song}
+						isPlaying={isPlaying}
+						activeSong={activeSong}
+						data={data}
+						i={i}
+					/>
+				))}
+			</div>
+		</div>
+	);
+};
 
 export default CountryTracks;
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -24,6 +24,9 @@ export const shazamCoreApi = createApi({
 		getArtistDetails: builder.query({
 			query: (artistId) => `/v2/artists/details?artist_id=${artistId}`,
 		}),
+		getSongsByCountry: builder.query({
+			query: (countryCode) => `/v1/charts/country?country_code=${countryCode}`,
+		}),
 	}),
 });
 
@@ -32,4 +35,5 @@ export const {
 	useGetSongDetailsQuery,
 	useGetSongRelatedQuery,
 	useGetArtistDetailsQuery,
+	useGetSongsByCountryQuery,
 } = shazamCoreApi;
